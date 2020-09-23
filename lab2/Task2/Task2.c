6f5ebbf66a1e75fb34ccb50a67edc5c6b24eed7d
DaggeNRoll <71384493+DaggeNRoll@users.noreply.github.com>
2020-09-23 22:55:49 +0300

    .cpp -> .c + шаблон второго задания

---
+++ lab2/Task2/Task2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+void main() {
+	setlocale(LC_ALL, "Rus");
+	float a, b, c, d;
+	printf_s("a(x)^2+bx+c=0\n");
+	printf_s("введите коэффициент a:");
+	scanf_s("%f", &a);
+	printf_s("введите коэффициент b:");
+	scanf_s("%f", &b);
+	printf_s("введите свободный член c: ");
+	scanf_s("%f", &c);
+	d = b * b - 4 * a * c;
+	if (d > 0) {
+		printf_s("x1= %f\n", (-b + sqrt(d)) / (2 * a));
+		printf_s("x2= %f\n", (-b - sqrt(d)) / (2 * a));
+	}
+	else if (d == 0) {
+		printf_s("x1=x2= %f", (-b) / (2 * a));
+	}
+	else {
+		printf_s("Действительных корней нет");
+	}
+}
