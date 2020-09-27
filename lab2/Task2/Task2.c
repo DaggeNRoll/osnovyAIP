7f29d34ed4f6fa57393cf57888502c7115d8a875
DaggeNRoll <71384493+DaggeNRoll@users.noreply.github.com>
2020-09-27 20:56:43 +0300

    Task2

--- lab2/Task2/Task2.c
+++ lab2/Task2/Task2.c
@@ -1,25 +1,35 @@
-#include <stdio.h>
+п»ї#include <stdio.h>
 #include <math.h>
 #include <locale.h>
 void main() {
-	setlocale(LC_ALL, "Rus");
-	float a, b, c, d;
+	setlocale(LC_ALL, "RUS");
+	float a, b, c, d, x1, x2, s;
 	printf_s("a(x)^2+bx+c=0\n");
-	printf_s("введите коэффициент a:");
+	printf_s("РІРІРµРґРёС‚Рµ РєРѕСЌС„С„РёС†РёРµРЅС‚ a!=0:");
 	scanf_s("%f", &a);
-	printf_s("введите коэффициент b:");
+	printf_s("РІРІРµРґРёС‚Рµ РєРѕСЌС„С„РёС†РёРµРЅС‚ b:");
 	scanf_s("%f", &b);
-	printf_s("введите свободный член c: ");
+	printf_s("РІРІРµРґРёС‚Рµ СЃРІРѕР±РѕРґРЅС‹Р№ С‡Р»РµРЅ c: ");
 	scanf_s("%f", &c);
 	d = b * b - 4 * a * c;
 	if (d > 0) {
-		printf_s("x1= %f\n", (-b + sqrt(d)) / (2 * a));
-		printf_s("x2= %f\n", (-b - sqrt(d)) / (2 * a));
+		if (modff(sqrt(d), &s) == 0) {
+			printf_s("x1= %f\n", (-b + sqrt(d)) / (2 * a));
+			printf_s("x2= %f\n", (-b - sqrt(d)) / (2 * a));
+		}
+		else {
+			printf_s("x1= (%f", -b);
+			printf_s(" + sqrt(%f", d);
+			printf_s(")) / %f", 2 * a);
+			printf_s("\nx1= (%f", -b);
+			printf_s(" - sqrt(%f", d);
+			printf_s(")) / %f", 2 * a);
+		}
 	}
 	else if (d == 0) {
 		printf_s("x1=x2= %f", (-b) / (2 * a));
 	}
 	else {
-		printf_s("Действительных корней нет");
+		printf_s("Р”РµР№СЃС‚РІРёС‚РµР»СЊРЅС‹С… РєРѕСЂРЅРµР№ РЅРµС‚");
 	}
 }
