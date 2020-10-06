ee0fe369d28b09d7eb30355ed028422f48871a92
DaggeNRoll <71384493+DaggeNRoll@users.noreply.github.com>
2020-10-06 19:18:24 +0300

    Коменты

--- Lab4/Task1/Source.c
+++ Lab4/Task1/Source.c
@@ -1,27 +1,22 @@
-#include <stdio.h>
-#include <locale.h>
+#include <stdio.h>//ввод-вывод
+#include <locale.h>//Локализация
 int main() {
 	setlocale(LC_ALL, "RUS");
 	int i, j, n, arr[] = { 1,8,2,10,34,56,46,23 };
-	printf_s("Массив до работы алгоритма: ");
-	n = sizeof(arr) / sizeof(int);
-	for (i = 0; i < n; i++) {
-		printf_s("%d", arr[i]);
-		printf_s(" ");
-	}
+	//объявление переменных и динамического массива
+	n = sizeof(arr) / sizeof(int);//определение
+	//размера массива
 	for (i = 0; i < n - 1; i++) {
+		//основной проход по массиву
 		for (j = 0; j < n - 1 - i; j++) {
+//внутренний проход по элементам, обеспечивающий
+//их сравнение и перестановку (при надобности)
 			if (arr[j] > arr[j + 1]) {
 				int buf = j+1;
 				arr[j+1] = arr[j];
 				arr[j] = buf;
 			}
 		}
-	}
-	printf_s("Массив после работы алгоритма: ");
-	for (i = 0; i < n; i++) {
-		printf_s("%d", arr[i]);
-		printf_s(" ");
 	}
 	return 0;
 }
