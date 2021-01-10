560b545b059bd248eae1980f548badf97affea1e
Mikhail Trifonov <71384493+DaggeNRoll@users.noreply.github.com>
2021-01-10 21:45:05 +0300

    Update 2.c

--- Vstavka/While/2.c
+++ Vstavka/While/2.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main() {
-	int i, n, x, j, array[] = { 10,2,3,5,3,1 };
+	int i=1, n, x, j, array[] = { 10,2,3,5,3,1 };
 	n = sizeof(array) / sizeof(int);
 	
-	for (i = 1; i < n; i++) {
+	while(i<n) 
+	{
 		x = array[i];
 		j = i;
 		while (j > 0 && array[j - 1] < x) {
 			array[j - 1] = array[j];
 			j--;
 		}
+		i++;
 	}
 
 	for (i = 0; i < n; i++) {
