8610bbc0f80a972363fdf154491f1520ced3b37a
Mikhail Trifonov <71384493+DaggeNRoll@users.noreply.github.com>
2021-01-11 23:39:52 +0300

    Update 2.c
    no swapping

--- Vstavka/While/2.c
+++ Vstavka/While/2.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	int i=1, n, x, j, array[] = { 10,2,3,5,3,1 };
+	int i=1, temp, n, x, j, array[] = { 10,2,3,5,3,1 };
 	n = sizeof(array) / sizeof(int);
 	
 	while(i<n) 
 	{
 		x = array[i];
 		j = i;
-		while (j > 0 && array[j - 1] < x) {
+		while (j > 0 && array[j - 1] > x) {
+			temp = array[j-1];
 			array[j - 1] = array[j];
+			array[j] = array[j-1];
 			j--;
 		}
 		i++;
